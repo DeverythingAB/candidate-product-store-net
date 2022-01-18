3b5aa153237add9f2efa805759f939fce3d2aa6a
Deverything <90911057+DeverythingAB@users.noreply.github.com>
2022-01-18 08:23:35 +0100

    Add files via upload

---
+++ ICandidateProductStore.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CandidateProductStore.Template
+{
+    /// <summary>
+    /// Suggestion of a interface for the implementation of the candidate product store candidate test
+    /// </summary>
+    interface ICandidateProductStore
+    {
+        /// <summary>
+        /// Get all products
+        /// </summary>
+        public IList<Product> GetAllProducts();
+
+
+        /// <summary>
+        /// Get all products above given price
+        /// </summary>
+        /// <param name="price"></param>
+        /// <returns></returns>
+        public IList<Product> GetAllProductsAbovePrice(decimal price);
+
+
+        /// <summary>
+        /// Gets the smallest box  to fit two products, can handle different sizes and returns the smallest one available, boxes can be rotated 90 degrees
+        /// </summary>
+        /// <param name="product1"></param>
+        /// <param name="product2"></param>
+        /// <returns></returns>
+        public Box CalculateSmallestBoxForTwoProducts(Product product1, Product product2);
+
+        /// <summary>
+        /// Checkout the smallest box and the two boxes containing them
+        /// </summary>
+        /// <param name="box"></param>
+        /// <param name="product1"></param>
+        /// <param name="product2"></param>
+        /// <returns></returns>
+        public CheckoutSummary Checkout(Box box, Product product1, Product product2);
+
+    }
+}
---
+++ ICandidateProductStoreApi.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CandidateProductStore.Template
+{
+    public record Product();
+    public record ProductDimensions();
+    public record Box();
+    public record CheckoutSummary();
+
+
+    /// <summary>
+    /// Suggested interface for the REST Api connection
+    /// </summary>
+    public interface ICandidateProductStoreApi
+    {
+        /// <summary>
+        /// Retrieve all products from the REST Api
+        /// </summary>
+        public IList<Product> GetProducts();
+
+        /// <summary>
+        /// Retrieve the dimensions of a product from the REST Api
+        /// </summary>
+        /// <param name="productId">Product id</param>
+        public ProductDimensions GetProductDimensions(int productId);
+
+        /// <summary>
+        /// Retrieve all the boxes and their dimensions from the REST Api
+        /// </summary>
+        public IList<Box> GetBoxes();
+
+        /// <summary>
+        /// Validates and performs the order if correct
+        /// </summary>
+        /// <param name="boxId"></param>
+        /// <param name="productIds"></param>
+        /// <returns>A summary of the order and if it was valid</returns>
+        public CheckoutSummary Checkout(int boxId, int[] productIds);
+
+
+
+    }
+}
